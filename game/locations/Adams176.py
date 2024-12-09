1b982932b25c7504b0d6df512287967cf56814dd
Adam176 <none@none.none>
2024-12-09 16:42:15 -0500

    working

--- game/locations/Adams176.py
+++ game/locations/Adams176.py
@@ -50,7 +50,10 @@ class BeachWithShip (location.SubLocation):
         if (verb == "north"):
             config.the_player.next_loc = self.main_location.locations["sub"]
 
-
+class coordinate:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
 
 class Subarea (location.SubLocation):
@@ -58,6 +61,7 @@ class Subarea (location.SubLocation):
         super().__init__(m)
         self.name = "southBeach"
         self.verbs['south'] = self
+        self.played_times = 0
 
 
     def enter (self):
@@ -70,60 +74,102 @@ class Subarea (location.SubLocation):
             self.main_location.end_visit()
 
     def reactiongame(self):
-        command_list = ['w', 'a', 's', 'd']
-        x_times = 0
-        points = 0
-        display.announce('Dodge 8 out of 10 incoming spike balls by pressing coressponding keys in time')
-        time.sleep(1)
-        while x_times < 10:
-            rand_spot = random.randint(0, 3)
-            #   Seconds part
-            startime = time.time()
-            reaction = display.get_text_input(f'Press {command_list[rand_spot]} ')
-            #   Seconds part
-            endtime = time.time()
-            delta = endtime - startime
-            if (reaction == command_list[rand_spot]) and delta <= 1.5:
-                points += 1
-            else:
-                display.announce('You got hit')
-                piratelist = config.the_player.get_pirates()
-                random.shuffle(piratelist)
-                num = random.randint(1,len(piratelist) + 1)
-                targets = (piratelist[:num])
-                for hit in targets:
-                    display.announce(f'{hit.get_name()} got hit for 15 Hp')
-                    deader = hit.inflict_damage(15, 'smashed by a spike ball')
-                    #If a pirate is killed by the damage, inflict_damge returns them.
-                    # Otherwise, it returns None
-                    if not (deader is None):
-                        display.announce(f"{deader.get_name()} is killed!")
-                        config.the_player.cleanup_pirates() #Makes sure the game ends if all pirates are dead
-                    
-            x_times += 1
-        if points >= 8:
-            pass
+        self.played_times += 1
+        if self.played_times <= 3:
+            command_list = ['w', 'a', 's', 'd']
+            x_times = 0
+            points = 0
+            display.announce('Dodge 8 out of 10 incoming spike balls by pressing coressponding keys in time')
+            time.sleep(1)
+            while x_times < 10:
+                rand_spot = random.randint(0, 3)
+                #   Seconds part
+                startime = time.time()
+                reaction = display.get_text_input(f'Press {command_list[rand_spot]} ')
+                #   Seconds part
+                endtime = time.time()
+                delta = endtime - startime
+                if (reaction == command_list[rand_spot]) and delta <= 1.5:
+                    points += 1
+                else:
+                    display.announce('You got hit')
+                    piratelist = config.the_player.get_pirates()
+                    random.shuffle(piratelist)
+                    num = random.randint(1,len(piratelist) + 1)
+                    targets = (piratelist[:num])
+                    for hit in targets:
+                        display.announce(f'{hit.get_name()} got hit for 15 Hp')
+                        deader = hit.inflict_damage(15, 'smashed by a spike ball')
+                        #If a pirate is killed by the damage, inflict_damge returns them.
+                        # Otherwise, it returns None
+                        if not (deader is None):
+                            display.announce(f"{deader.get_name()} is killed!")
+                            config.the_player.cleanup_pirates() #Makes sure the game ends if all pirates are dead
+                        
+                x_times += 1
+            if points >= 8:
+                pass
+                #Give Speed and Accuracy points for completion 
+        else:
+            display.announce(f'You have maximized the limit of which you can enter the sub-area')
+                             
 
-            #Define getting hit. Should maybe take some health down? 
+            #Define getting hit. Should maybe take some health down?(Completed?) 
             #Figure out a prize for winning 
-            #Island Theme?
-            #if returned the 4th time, subarea doesn't work
+            #Island Theme?  
+            #if returned the 4th time, subarea doesn't work(Completed?)
     def matchingpuzzle(self):
-        pass
-        '''
         set_board = [
-            [1, 2, 3],
-            [4, 5, 6],
-            [7, 8, 9]
+            [" ", " ", " ", " "],
+            [" ", " ", " ", " "],
+            [" ", " ", " ", " "],
+            [" ", " ", " ", " "]
         ]
-        player = random_randint(1, 2)
-        spot = random_randint(1, 9)
-        if spot == range(1, 3):
-            if player == 1:
-                set_board[spot - 1][spot - 1] = X
-            if player == 2:
-                set_board[spot - 1][spot - 1] = O
+        playerXO = random.choice([1, 2])
+        
+        x = random.choice(range(4))
+        y = random.choice(range(4))
+        set_board[y][x] = 'X'
+        X_Coord = coordinate(x,y)
+        while set_board[y][x] != " " or ((X_Coord.y == y) and (abs(x - X_Coord.x) == 1)): #note:guaranteed the first trip through the loop
+            x = random.choice(range(4))
+            y = random.choice(range(4))
+        #once we're past the loop, x,y is a good spot
+        set_board[y][x] = 'O'
+        O_Coord = coordinate(x,y)
+        #Get input
+        # w a s d
+        input_meanings = {}
+        input_meanings['w'] = coordinate(0,-1)
+        input_meanings['s'] = coordinate(0,1)
+        input_meanings['a'] = coordinate(-1,0)
+        input_meanings['d'] = coordinate(1,0)
+        line = ["─"]*len(set_board)
+        print("┌"+"┬".join(line)+"┐")
+        line = "├"+"┼".join(line)+"┤"
+        for row in set_board:
+            print("│"+"│".join(row)+"│")
+            print(line)
+        #etc
+
+        #Make either X or O move
+        if playerXO == 1:
+            playerXO = 'X'
+            player = X_Coord
+            alt = O_Coord
+        else:
+            playerXO = 'O'
+            player = O_Coord
+            alt = X_Coord
+        display.announce(f"Control the '{playerXO}' using 'w a s d' to line up with the '{alt}' side by side.")
+        #Reward with Treasure or Stat upgrade
+        user_input = input()
+        if user_input in input_meanings:
+            move = input_meanings[user_input]
+            X_Coord = " "
+            X_Coord - move = playerXO 
+        else:
+            display.announce("Use 'w a s d' to move.")
 
 
 #############################################
-'''
